cc8fe7df4f7fa737d5265cae6d6d3a0a48cfaee0
xsp <xusupeng@beisen.com>
2021-04-02 18:45:00 +0800

    Java多线程相关学习

--- Concurrent/src/main/java/com/xsp/Lock/ReentrantLock/ReentrantXexample.java
+++ Concurrent/src/main/java/com/xsp/Lock/ReentrantLock/ReentrantXexample.java
@@ -1,4 +1,11 @@
 package com.xsp.Lock.ReentrantLock;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ReentrantXexample {
+    static ReentrantLock a = new ReentrantLock();
+
+    public static void main(String[] args) {
+        a.lock();
+    }
 }
